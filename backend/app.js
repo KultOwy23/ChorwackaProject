f5a34684cfd4153f6d80fa1c4ccd279aabd164a3
KultOwy23 <jedrzej.zawojski95@gmail.com>
2020-02-08 02:26:29 +0100

    Some first routes

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,10 @@ const app = express()
 const host = process.argv[2]
 const port = process.argv[3]
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/month', (req, res) => res.send('This month: February'))
+app.get('/ads', (req, res) => res.send("What`s new"))
+app.get('/shopping', (req, res) => res.send('To buy'))
+app.get('/cleaning/:id', (req, res) => res.send(`Cleaning schedule ${req.params.id}`))
+
 
 app.listen(port, host, () => console.log(`Example app listening on port ${host}:${port}`))
